57b7e8ee3cb6928fcc03a987fd5031f57c01e1e5
vladg <vlad.grush.99@gmail.com>
2023-09-23 12:04:21 +0300

    init project

---
+++ SqlPractice/DbConnections/AppNpgsqlConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Npgsql;
+
+namespace SqlPractice.DbConnections;
+
+public class AppNpgsqlConnection
+{
+    private readonly IDbConnection _connection;
+
+    public AppNpgsqlConnection(string connectionString)
+    {
+        _connection = new NpgsqlConnection(connectionString);
+    }
+    public IDbConnection GetConnection()
+    {
+        return _connection;
+    }
+
+    public async Task InitDatabase()
+    {
+        
+    }
+
+    public async Task CreateTables()
+    {
+        
+    }
+
+    public async Task InitData()
+    {
+        
+    }
+}
