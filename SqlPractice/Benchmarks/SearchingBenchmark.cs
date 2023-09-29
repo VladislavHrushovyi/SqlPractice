2e38cebfb5d73523066bab9da1e5cd8ed26cf6ab
vladg <vlad.grush.99@gmail.com>
2023-09-29 15:52:34 +0300

    create new class for benchmarking

---
+++ SqlPractice/Benchmarks/SearchingBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using SqlPractice.DbConnections;
+
+namespace SqlPractice.Benchmarks;
+
+public class SearchingBenchmark
+{
+    private readonly NpgsqlConnection _connection;
+    private const string ConnectionString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=TestDb";
+    public SearchingBenchmark()
+    {
+        var connectionInstance = new AppNpgsqlConnection(ConnectionString);
+        this._connection = connectionInstance.CreateConnection();
+    }
+
+    public async Task SearchByDefault()
+    {
+        
+    }
+    
+    public async Task SearchByIndexCreation()
+    {
+        
+    }
+}
